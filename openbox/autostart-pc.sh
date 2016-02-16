d487e6bfa444e872812fef7f9e80f9dcb2d36525
Rafael Ketsetsides <rafael859@gmail.com>
2016-02-16 18:27:08 +0200

    Added configurations from pc

---
+++ openbox/autostart-pc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+tint2 &
+feh --bg-scale ~/Pictures/Wallpapers/mybabe.jpg
+setxkbmap -layout "us,el" -option "grp:alt_shift_toggle" &
+(sleep 1 && volumeicon) &
+(sleep 1 && xscreensaver --no-splash) &
