93271747eec745b7b0f2ce840175932a02edb631
Rafael Ketsetsides <rafael859@gmail.com>
2016-02-17 17:07:25 +0200

    Fixed xscreensaver startup

--- openbox/autostart-pc.sh
+++ openbox/autostart-pc.sh
@@ -3,4 +3,4 @@ tint2 &
 feh --bg-scale ~/Pictures/Wallpapers/mybabe.jpg
 setxkbmap -layout "us,el" -option "grp:alt_shift_toggle" &
 (sleep 1 && volumeicon) &
-(sleep 1 && xscreensaver --no-splash) &
+(sleep 1 && xscreensaver -no-splash) &
